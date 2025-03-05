69c1edbfbfb0d3c6977d49ed33c3f85f40630efe
Sanjida Akter <sanjidakter2304@gmail.com>
2025-03-06 01:58:59 +0600

    frontend homepage complete

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# medicare-booking" 
+
+- A website for booking medical appointments
+- Shows review of patients
+- Get available doctors list
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,11 +7,14 @@ import icon3 from "../assets/images/icon03.png";
 import featureImg from "../assets/images/feature-img.png";
 import videoIcon from "../assets/images/video-icon.png";
 import avatarIcon from "../assets/images/avatar-icon.png";
+import faqImg from "../assets/images/faq-img.png";
 import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
 import About from "../components/About/About";
 import ServiceList from "../components/Services/ServiceList";
 import DoctorsList from "./Doctors/Doctors/DoctorsList";
+import FaqList from "../components/Faq/FaqList";
+import Testimonial from "../components/Testimonial/Testimonial";
 
 const Home = () => {
   return (
@@ -266,6 +269,37 @@ const Home = () => {
           </div>
         </section>
         {/* =========== our great doctros end ================== */}
+
+        {/* =========== faq section ================ */}
+        <section>
+          <div className="container">
+            <div className="flex justify-between gap-[50px] lg:gap-0">
+              <div className="w-1/2 hidden md:block">
+                <img src={faqImg} alt="" />
+              </div>
+
+              <div className="w-full md:w-1/2">
+                <h2 className="heading">
+                  Most questions by our beloved patients
+                </h2>
+                <FaqList />
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* =========== faq section end ================ */}
+
+        {/* ========== testimonial ========== */}
+        <section>
+          <div className="container">
+            <div className="xl:w-[470px] mx-auto">
+              <h2 className="heading text-center">What our patient say</h2>
+              <p className="text__para text-center"> world-class care for everyone. Our health system offers unmatched, expert health care.</p>
+            </div>
+            <Testimonial/>
+          </div>
+        </section>
+        {/* ========== testimonial end ========== */}
       </>
     </>
   );
